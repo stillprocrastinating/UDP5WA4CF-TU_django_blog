1136e31e90b657f21150bb424a976b8cbdd2e57d
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-12-23 23:00:43 +0000

    Post() > __str__()

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,9 @@ class Post(models.Model):
     
     class Meta:
         ordering = ["-created_on"]
+    
+    def __str__(self):
+        return f"The title of this post is {self.title}"
 
 
 class Comment(models.Model):
