538093a7cb6f94b2ebe78e3c014a1377c767621b
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-12-23 22:59:01 +0000

    Post() > Meta

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,9 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["-created_on"]
 
 
 class Comment(models.Model):
