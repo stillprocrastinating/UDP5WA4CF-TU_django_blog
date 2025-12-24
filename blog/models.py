12bab4607be0c4da7658e6f7bf4b2abbb2c8c521
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-12-24 08:01:56 +0000

    EXAMPLE python manage.py makemigrations

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    field_2 = models.CharField(default="Hello, World!")
+    field_2 = models.IntegerField(default=42)
     field_3 = models.CharField(null=True)
     
     class Meta:
