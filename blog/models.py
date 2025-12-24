89f68e43accdd6b8e436da16d1766b7bf43ec4c2
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-12-24 07:54:37 +0000

    python manage.py makemigrations

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,6 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    field_1 = models.CharField()
     
     class Meta:
         ordering = ["-created_on"]
