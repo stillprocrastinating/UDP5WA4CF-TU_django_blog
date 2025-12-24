c28587abe2df3e71ff79295b6c5428f2e4d9fad0
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-12-24 09:54:34 +0000

    CHALLENGE FloatField(default)

--- blog/models.py
+++ blog/models.py
@@ -28,7 +28,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    challenge = models.SlugField(default='slug_challenge')
+    challenge = models.FloatField(default=3.0)
 
     class Meta:
         ordering = ["created_on"]
