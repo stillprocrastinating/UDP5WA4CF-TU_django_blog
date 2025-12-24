6d40796a516727ba11fdfc9ed41216557a4e500c
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-12-24 10:17:42 +0000

    CHALLENGE python manage.py migrate blog zero --> rm blog/migrations/0006_comment_challenge.py --> python manage.py makemigrations

--- blog/models.py
+++ blog/models.py
@@ -28,7 +28,6 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    challenge = models.FloatField(default=3.0)
 
     class Meta:
         ordering = ["created_on"]
