4d96844f186ce374dab1958001b560787c4de201
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-01-08 22:38:21 +0000

    context

--- blog/views.py
+++ blog/views.py
@@ -26,8 +26,10 @@ def post_detail(request, slug):
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
 
+    context = {"post": post}
+
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        context,
     )
