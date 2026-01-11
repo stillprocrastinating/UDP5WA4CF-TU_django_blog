d61efee1373308bd141edef210e2aa1ca681d6f5
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-01-11 19:29:39 +0000

    comment_delete

--- blog/views.py
+++ blog/views.py
@@ -88,5 +88,30 @@ def comment_edit(request, slug, comment_id):
                 messages.ERROR,
                 "Error updating comment!"
             )
-    
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    View to delete comments
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            'Comment deleted!'
+        )
+    else:
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'You can only delete your own comments!'
+        )
+
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
