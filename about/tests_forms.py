d75bff1430bb4ad34da645bb2751e219bee95eb1
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-01-14 11:59:54 +0000

    TestCollaborateForm()

---
+++ about/tests_forms.py
@@ -0,0 +1,76 @@
+from django.test import TestCase
+from .forms import CollaborateForm
+
+
+class TestCollaborateForm(TestCase):
+    """
+    Docstring for TestCollaborateForm
+    """
+
+    def test_form_is_valid(self):
+        """
+        Test for all fields.
+
+        :param self: Description
+        """
+
+        form = CollaborateForm({
+            'name': 'Matt',
+            'email': 'test@test.com',
+            'message': 'Hello!'
+        })
+        self.assertTrue(
+            form.is_valid(),
+            msg='Form is invalid'
+        )
+
+    def test_name_is_required(self):
+        """
+        Test for the 'name' field.
+
+        :param self: Description
+        """
+
+        form = CollaborateForm({
+            'name': '',
+            'email': 'test@test.com',
+            'message': 'Hello!'
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg='Name was not provided but the form is valid'
+        )
+
+    def test_email_is_required(self):
+        """
+        Test for the 'email' field.
+
+        :param self: Description
+        """
+
+        form = CollaborateForm({
+            'name': 'Matt',
+            'email': '',
+            'message': 'Hello!'
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg='Email was not provided but the form is valid'
+        )
+
+    def test_message_is_required(self):
+        """
+        Test for the 'message' field
+
+        :param self: Description
+        """
+
+        form = CollaborateForm({
+            'name': 'Matt',
+            'email': 'test@test.com',
+            'message': ''
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg='Message was not provided but the form is valid'
+        )
