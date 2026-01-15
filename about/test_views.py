d1f5406fd18c73612bd3ddbba6dd163871d6a2b2
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-01-15 08:10:11 +0000

    TestAboutView() > test_successful_collaboration_request_submission()

--- about/test_views.py
+++ about/test_views.py
@@ -42,3 +42,28 @@ class TestAboutView(TestCase):
             response.context['collaborate_form'],
             CollaborateForm
         )
+
+    def test_successful_collaboration_request_submission(self):
+        """
+        Test for a user requesting a collaboration
+        """
+
+        post_data = {
+            'name': 'test name',
+            'email': 'test@email.com',
+            'message': 'test message'
+        }
+        response = self.client.post(
+            reverse(
+                'about'
+            ),
+            post_data
+        )
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+        self.assertIn(
+            b'Collaboration request received! I endeavour to respond within 2 working days.',
+            response.content
+        )
