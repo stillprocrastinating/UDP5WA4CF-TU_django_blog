127baae90b71bd79f4b0f73a6f9688526933a5c2
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-01-15 08:03:35 +0000

    TestBlogViews() > test_successful_comment_submission()

--- blog/test_views.py
+++ blog/test_views.py
@@ -63,3 +63,31 @@ class TestBlogViews(TestCase):
             response.context['comment_form'],
             CommentForm
         )
+
+    def test_successful_comment_submission(self):
+        """
+        Test for posting a comment on a post
+        """
+
+        self.client.login(
+            username="myUsername",
+            password="myPassword"
+        )
+        post_data = {
+            'body': 'This is a test comment.'
+        }
+        response = self.client.post(
+            reverse(
+                'post_detail',
+                args=['blog-title']
+            ),
+            post_data
+        )
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+        self.assertIn(
+            b'Comment submitted and awaiting approval',
+            response.content
+        )
